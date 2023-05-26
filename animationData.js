bab646aad3443793b6e3859e08355b540e53b1c6
Seankuwamoto <seanbun9000@icloud.com>
2023-05-26 12:54:28 -0700

    start of the website

---
+++ animationData.js
@@ -0,0 +1,23 @@
+const animationData = {
+    "percentContainer": {
+        "timings": [0, 10, 20, 100],
+        "relativeTranslate": [[-50, -50], [-50, -50], [0, 0], [0, 0]],
+        "translate": [[0,0], [0, 0], [-1, -1], [-1, -1]],
+        "scale": [2, 2, 1, 1],
+        "opacity": [1, 1, 1, 1]
+    },
+    "informationContainer": {
+        "timings": [0, 45, 55, 65, 100],
+        "relativeTranslate": [[-50, 50], [-50, 50], [-50, -50], [-50, -150], [-50, -50]],
+        "translate": [[0, 0], [0, 0], [0, 0], [0, 0], [0, 0]],
+        "scale": [1, 1, 1, 1, 1],
+        "opacity": [0, 0, 1, 0, 0]
+    },
+    "informationContainer2": {
+        "timings": [0, 60, 70, 80, 100],
+        "relativeTranslate": [[-50, 50], [-50, 50], [-50, -50], [-50, -150], [-50, -50]],
+        "translate": [[0, 0], [0, 0], [0, 0], [0, 0], [0, 0]],
+        "scale": [1, 1, 1, 1, 1],
+        "opacity": [0, 0, 1, 0, 0]
+    }
+}
---
+++ portfolio.js
@@ -0,0 +1,71 @@
+// Scroll handlers
+document.addEventListener('scroll', onScroll);
+window.addEventListener('resize', onScroll);
+
+// This function also gets called on window resize.
+function onScroll() {
+
+    // viewport width and height
+    const viewportWidth = window.innerWidth;
+    const viewportHeight = window.innerHeight;
+
+    // Current scroll position
+    const scroll = window.scrollY;
+
+    // Maximum scroll position
+    const maxScroll = document.body.scrollHeight - window.innerHeight;
+
+    // Percentage of scroll (rounded to 1 decimal place)
+    const percentComplete = Math.min(Math.round(scroll/maxScroll * 1000) / 10, 100);
+    
+    // Text box for percent complete
+    const percentText = document.getElementById('percent');
+    percentText.innerHTML = percentComplete + "%";
+
+    // Positon and scale animations.
+    const animationElements = ['percentContainer', 'informationContainer', 'informationContainer2'];
+
+    for (let id of animationElements) {
+        const element = document.getElementById(id);
+        // Name shortening
+        const dat = animationData[id];
+        let timingIndex = 0;
+        let timing = 0;
+        if (percentComplete == 100) {
+            timing = 1;
+            timingIndex = dat.timings.length - 1;
+        }
+        else {
+            // Goes through the animation timings and finds the current timing.
+            while (percentComplete > dat.timings[timingIndex + 1]) timingIndex++;
+            timing = (percentComplete - dat.timings[timingIndex]) / (dat.timings[timingIndex + 1] - dat.timings[timingIndex]);
+        }
+        // Applies translation and scale effects.
+        element.style.transform = `
+        translate(
+            ${smoothLerp(dat.relativeTranslate[timingIndex][0], dat.relativeTranslate[timingIndex + 1][0], timing)}%,
+            ${smoothLerp(dat.relativeTranslate[timingIndex][1], dat.relativeTranslate[timingIndex + 1][1], timing)}%
+        )
+        translate(
+            ${smoothLerp(dat.translate[timingIndex][0], dat.translate[timingIndex + 1][0], timing) * viewportWidth/2}px,
+            ${smoothLerp(dat.translate[timingIndex][1], dat.translate[timingIndex + 1][1], timing) * viewportHeight/2}px
+        )
+        scale(
+            ${smoothLerp(dat.scale[timingIndex], dat.scale[timingIndex + 1], timing)}
+        )`;
+        // Applies opacity effects.
+        element.style.opacity = smoothLerp(dat.opacity[timingIndex], dat.opacity[timingIndex + 1], timing);
+
+    }
+
+}
+
+function smoothLerp(x, y, t) {
+    return smoothstep(t) * (y - x) + x;
+}
+
+function smoothstep(t) {
+    // if (t <= 0) return 0;
+    // if (t >= 1) return 1;
+    return t * t * (3 - 2 * t);
+}
